59271aecd65018c512cefdd878978938f3dca5af
excelenz <margolin.dm@gmail.com>
2021-04-05 06:13:42 +0300

    first one

---
+++ config.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+import os
+
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////tmp/db.sqlite3"
+db = SQLAlchemy(app)
+
+
+SQLALCHEMY_ECHO = False
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/db.sqlite3"
+DATABASE_URL = "sqlite:////tmp/db.sqlite3"
+
+
+SECRET_KEY = "sdsdsdsdsd-dsdsd-sdshgf£32423423443"
---
+++ models.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from marshmallow import Schema, fields, pre_load, validate,post_dump
+from flask_marshmallow import Marshmallow
+from flask_sqlalchemy import SQLAlchemy
+import datetime
+
+ma = Marshmallow()
+db = SQLAlchemy()
+
+
+class Tasks(db.Model):
+    __tablename__ = 'tasks'
+    task_id = db.Column(db.Integer, primary_key=True)
+    task_name = db.Column(db.String(255))
+    time_create = db.Column(db.String)
+    status = db.Column(db.Integer, default = 0)
+    def __repr__(self):
+        return '<Tasks {}>'.format(self.task_name)
+
+class TasksSchema(ma.Schema):
+    task_id = fields.Int()
+    task_name = fields.Str()
+    status = fields.Int()
+    time_create = fields.Method("format_date", dump_only=True)
+    ordered=True
+    def format_date(self, Tasks):
+        return datetime.datetime.strptime(Tasks.time_create, '%Y-%m-%d,%H:%M')
